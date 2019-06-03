5aa6fd8931521e7e6afde997b770d5f44cd13c33
kasuga19606 <s190606@tokyo.kosen-ac.jp>
2019-06-03 13:06:28 +0900

    server listen HTTP GET
    the application is listening for HTTP GET on the root / and on the / test rootes

---
+++ index.js
@@ -0,0 +1,14 @@
+'user  strict';
+
+const express = require('express');
+const app  = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+app.get('/test', (req, res) => {
+    res.send('Testing is fun');
+});
+
+app.listen(3000);
